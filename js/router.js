8c6b47ab888711b7b5e8b26137cec34b4e563a06
codewld <wang_longda@outlook.com>
2021-11-08 21:41:29 +0800

    优化router.js

--- js/router.js
+++ js/router.js
@@ -2,6 +2,7 @@
 
     function Router() {
         this.routerMap = {}
+        this.pathParams = []
     }
 
     /**
@@ -22,7 +23,57 @@
                 onHashChange()
             })
         })
+        /**
+         * 获取url中的路径参数并进行匹配
+         */
+        function onHashChange() {
+            this.pathParams = getPathParams()
 
+            matchAndLoad(0, {
+                son: routerMap
+            })
+
+            /**
+             * 获取路径参数组成的数组
+             */
+            function getPathParams() {
+                let hash = location.hash.slice(location.hash.indexOf("#") + 1)
+                if (hash.charAt(0) === "/") {
+                    hash = hash.slice(1)
+                }
+                if (hash.charAt(hash.length - 1) === "/") {
+                    hash = hash.slice(0, hash.length - 1)
+                }
+                return hash.split("/")
+            }
+        }
+
+
+
+        /**
+         * 匹配路由并加载组件
+         * 使用递归方式，保证元素加载完成后再继续下一步
+         * @param matchRes 匹配结果
+         */
+        function matchAndLoad(i, matchRes) {
+            if (i < this.pathParams.length) {
+                if (matchRes.son.hasOwnProperty(this.pathParams[i])) {
+                    // 匹配路径表（根据重定向与否）
+                    if (matchRes.son[this.pathParams[i]].redirect !== undefined) {
+                        matchRes = matchRes.son[matchRes.son[this.pathParams[i]].redirect]
+                    } else {
+                        matchRes = matchRes.son[this.pathParams[i]]
+                    }
+
+                    // 加载匹配到的组件
+                    $('#router-view-' + (i + 1)).load(matchRes.path, () => {
+                        matchAndLoad(i + 1, matchRes)
+                    })
+                } else {
+                    $('#router-view-1').load(routerMap["404"].path)
+                }
+            }
+        }
     }
 
     /**
@@ -32,60 +83,11 @@
         location.hash = location.hash + str
     }
 
-     /**
+    /**
      * 跳转路径
      */
-      Router.prototype.to = function (str) {
+    Router.prototype.to = function (str) {
         location.hash = str
-    }
-
-    /**
-     * 获取url中的路径参数并进行匹配
-     */
-    function onHashChange() {
-        // 获取url中的有效路径参数
-        let hash = location.hash.slice(location.hash.indexOf("#") + 1)
-        if (hash.charAt(0) === "/") {
-            hash = hash.slice(1)
-        }
-        if (hash.charAt(hash.length - 1) === "/") {
-            hash = hash.slice(0, hash.length - 1)
-        }
-
-        // 将路径参数转化为数组
-        pathParams = hash.split("/")
-
-        // 创建初始匹配结果
-        let matchRes = {
-            son: routerMap
-        }
-
-        matchAndLoad(0, matchRes)
-    }
-
-    /**
-     * 匹配路由并加载组件
-     * 使用递归方式，保证元素加载
-     * @param matchRes 匹配结果
-     */
-    function matchAndLoad(i, matchRes) {
-        if (i < pathParams.length) {
-            if (matchRes.son.hasOwnProperty(pathParams[i])) {
-                // 匹配路径表（根据重定向与否）
-                if (matchRes.son[pathParams[i]].redirect !== undefined) {
-                    matchRes = matchRes.son[matchRes.son[pathParams[i]].redirect]
-                } else {
-                    matchRes = matchRes.son[pathParams[i]]
-                }
-
-                // 加载匹配到的组件
-                $('#router-view-' + (i + 1)).load(matchRes.path, () => {
-                    matchAndLoad(i + 1, matchRes)
-                })
-            } else {
-                $('#router-view-1').load(routerMap["404"].path)
-            }
-        }
     }
 
     window.$router = new Router()
