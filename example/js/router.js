391b7aeec5c0aa8da5cd25ff467d93686cd8076d
codewld <wang_longda@outlook.com>
2022-09-02 12:24:37 +0800

    避免重复load

--- example/js/router.js
+++ example/js/router.js
@@ -18,16 +18,49 @@
                 return
             }
             jQuery(() => onHashChange())
-            window.addEventListener('hashchange', () => {
-                jQuery(() => onHashChange())
+            window.addEventListener('hashchange', hashchangeEvent => {
+                jQuery(() => onHashChange(hashchangeEvent))
             })
 
             /**
              * 获取url中的路径参数并进行匹配
              */
-            const onHashChange = () => {
+            const onHashChange = (hashchangeEvent) => {
+                let newPathList = []
+                let oldPathList = []
+                let pathInfoList = []
+                if (hashchangeEvent) {
+                    newPathList = getPathList(hashchangeEvent.newURL)
+                    oldPathList = getPathList(hashchangeEvent.oldURL)
+                } else {
+                    newPathList = getPathList(location.hash)
+                }
+                for (let i = 0, startChange = false; i < newPathList.length; i++) {
+                    let isChange = !(startChange === false && i < oldPathList.length && oldPathList[i] === newPathList[i])
+                    if (isChange) {
+                        startChange = true
+                    }
+                    let pathInfo = {
+                        path: newPathList[i],
+                        isChange: isChange
+                    }
+                    pathInfoList.push(pathInfo)
+                }
+
+                // 创建初始匹配结果
+                let matchRes = {
+                    son: this.routerMap
+                }
+
+                matchAndLoad(1, matchRes, pathInfoList)
+            }
+
+            /**
+             * 将URL转化为路径数组
+             */
+            const getPathList = (url) => {
                 // 获取url中的有效路径参数
-                let hash = location.hash.slice(location.hash.indexOf('#') + 1)
+                let hash = url.slice(url.indexOf('#') + 1)
                 if (hash.indexOf('?') !== -1) {
                     hash = hash.slice(0, hash.indexOf('?'))   
                 }
@@ -39,14 +72,9 @@
                 }
 
                 // 将路径参数转化为数组
-                this.pathParams = hash.split('/')
+                let pathList = hash.split('/')
 
-                // 创建初始匹配结果
-                let matchRes = {
-                    son: this.routerMap
-                }
-
-                matchAndLoad(0, matchRes)
+                return pathList
             }
 
             /**
@@ -54,22 +82,27 @@
              * 使用递归方式，保证元素加载
              * @param matchRes 匹配结果
              */
-            const matchAndLoad = (i, matchRes) => {
-                if (i >= this.pathParams.length) {
+            const matchAndLoad = (i, matchRes, pathInfoList) => {
+                if (i > pathInfoList.length) {
                     return
                 }
-                let tempMatchRes = matchRes.son[this.pathParams[i]]
+                let tempMatchRes = matchRes.son[pathInfoList[i - 1].path]
                 if (tempMatchRes) {
                     if (tempMatchRes.redirect) {
-                        matchRes = tempMatchRes.redirect
+                        matchRes = matchRes.son[tempMatchRes.redirect]
                     } else {
                         matchRes = tempMatchRes
                     }
 
+                    
                     // 加载匹配到的组件
-                    $('#router-view-' + (i + 1)).load(matchRes.path, () => {
-                        matchAndLoad(i + 1, matchRes)
-                    })
+                    if (pathInfoList[i - 1].isChange) {
+                        $('#router-view-' + i).load(matchRes.path, () => {
+                            matchAndLoad(i + 1, matchRes, pathInfoList)
+                        })
+                    } else {
+                        matchAndLoad(i + 1, matchRes, pathInfoList)
+                    }
                 } else {
                     $('#router-view-1').load(routerMap['404'].path)
                 }
